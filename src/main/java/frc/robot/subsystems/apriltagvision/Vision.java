35d78a8d31b551b70d8b301eea03cdee1f11ccdf
SHAM-ROCK-BOTICS <shamrockbotics@gmail.com>
2024-01-27 10:40:02 -0500

    updated vision

--- src/main/java/frc/robot/subsystems/apriltagvision/Vision.java
+++ src/main/java/frc/robot/subsystems/apriltagvision/Vision.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems.apriltagvision;
 
+import edu.wpi.first.networktables.DoubleArraySubscriber;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -11,7 +12,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Vision extends SubsystemBase {
   /** Creates a new Vision. */
 
-  NetworkTable table;
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
 
   double tv;
   double tx;
@@ -22,6 +23,7 @@ public class Vision extends SubsystemBase {
   double cl;
   double getpipe;
 
+  DoubleArraySubscriber pSub;
   double[] botpose;
   double[] botpose_wpiblue;
   double[] botpose_wpired;
@@ -32,50 +34,24 @@ public class Vision extends SubsystemBase {
   double tid;
 
   public Vision() {
-    table = NetworkTableInstance.getDefault().getTable("limelight");
-
-    updateTables();
+    pSub = table.getDoubleArrayTopic("targetpose_robotspace").subscribe(new double[6]);
+    targetpose_robotspace = new double[6];
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-    updateTables();
+    //System.out.println(getTagID());;
   }
 
   public void updateTables(){
-    double def = 0.0;
-    	// Get data from NetworkTable
-			// tshort	Sidelength of shortest side of the fitted bounding box (pixels)
-			// tlong	Sidelength of longest side of the fitted bounding box (pixels)
-			// thor		Horizontal sidelength of the rough bounding box (0 - 320 pixels)
-			// tvert	Vertical sidelength of the rough bounding box (0 - 320 pixels)
-			// camtran	Results of a 3D position solution, NumberArray: Translation (x,y,z) Rotation(pitch,yaw,roll) 
-			//camerapose_robotspace	  3D transform of the camera in the coordinate system of the robot (array (6))          
-      tv = table.getEntry("tv").getDouble(def); //Whether the limelight has any valid targets (0 or 1)
-      if (tv != 0.0){
-        tx = table.getEntry("tx").getDouble(def); //Horizontal Offset From Crosshair To Target (LL1: -27 degrees to 27 degrees | LL2: -29.8 to 29.8 degrees)
-        ty = table.getEntry("ty").getDouble(def); //Vertical Offset From Crosshair To Target (LL1: -20.5 degrees to 20.5 degrees | LL2: -24.85 to 24.85 degrees)
-        ta = table.getEntry("ta").getDouble(def); //Target Area (0% of image to 100% of image)
-        ts = table.getEntry("ts").getDouble(def); //Skew or rotation (-90 degrees to 0 degrees)
-        tl = table.getEntry("tl").getDouble(def); //The pipeline’s latency contribution (ms) Add at least 11ms for image capture latency.
-        cl = table.getEntry("cl").getDouble(def);
-        getpipe = table.getEntry("getpipe").getDouble(def); //True active pipeline index of the camera (0 .. 9)
-
-        botpose = table.getEntry("botpose").getDoubleArray(new double[6]); //Robot transform in field-space. Translation (X,Y,Z) Rotation(Roll,Pitch,Yaw), total latency (cl+tl)
-        botpose_wpiblue = table.getEntry("botpose_wpiblue").getDoubleArray(new double[6]); //Robot transform in field-space (blue driverstation WPILIB origin). Translation (X,Y,Z) Rotation(Roll,Pitch,Yaw), total latency (cl+tl)
-        botpose_wpired = table.getEntry("botpose_wpired").getDoubleArray(new double[6]); //Robot transform in field-space (red driverstation WPILIB origin). Translation (X,Y,Z) Rotation(Roll,Pitch,Yaw), total latency (cl+tl)
-        camerapose_targetspace = table.getEntry("camerapose_targetspace").getDoubleArray(new double[6]); //3D transform of the camera in the coordinate system of the primary in-view AprilTag (array (6))
-        targetpose_cameraspace = table.getEntry("targetpose_cameraspace").getDoubleArray(new double[6]); //3D transform of the primary in-view AprilTag in the coordinate system of the Camera (array (6))
-        targetpose_robotspace = table.getEntry("targetpose_robotspace").getDoubleArray(new double[6]); //3D transform of the primary in-view AprilTag in the coordinate system of the Robot (array (6))
-        botpose_targetspace = table.getEntry("botpose_targetspace").getDoubleArray(new double[6]); //3D transform of the robot in the coordinate system of the primary in-view AprilTag (array (6))
-        tid = table.getEntry("tid").getDouble(def); //ID of the primary in-view AprilTag
-      }
-    
-    
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+    targetpose_robotspace = pSub.get(new double[6]);
+    tv = table.getEntry("tv").getDouble(0);
+    tid = table.getEntry("tid").getDouble(0);
   }
 
   public boolean hasValidTarget(){
+    updateTables();
     boolean result = (tv != 0);
     return result;
   }
