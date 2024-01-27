01d49afdcf4b60bde80ebac473fa538a5351b739
SHAM-ROCK-BOTICS <shamrockbotics@gmail.com>
2024-01-26 20:00:27 -0500

    created vision subsystem

--- src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
@@ -17,7 +17,6 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.util.WPIUtilJNI;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -34,8 +33,8 @@ public class SwerveDrivetrain extends SubsystemBase {
 
 	public static final double FRONT_LEFT_VIRTUAL_OFFSET_RADIANS = 1.72;//-1.653; // adjust as needed so that virtual (turn) position of wheel is zero when straight
 	public static final double FRONT_RIGHT_VIRTUAL_OFFSET_RADIANS = -1.49;//-1.650; // adjust as needed so that virtual (turn) position of wheel is zero when straight
-	public static final double REAR_LEFT_VIRTUAL_OFFSET_RADIANS = -1.47;//-0.987; // adjust as needed so that virtual (turn) position of wheel is zero when straight
-	public static final double REAR_RIGHT_VIRTUAL_OFFSET_RADIANS = -0.40;//-0.39;//+1.021; // adjust as needed so that virtual (turn) position of wheel is zero when straight
+	public static final double REAR_LEFT_VIRTUAL_OFFSET_RADIANS = -1.47;;//-0.987; // adjust as needed so that virtual (turn) position of wheel is zero when straight
+	public static final double REAR_RIGHT_VIRTUAL_OFFSET_RADIANS = -0.39;//+1.021; // adjust as needed so that virtual (turn) position of wheel is zero when straight
 
 	public static final int GYRO_ORIENTATION = -1; // might be able to merge with kGyroReversed
 
@@ -81,7 +80,7 @@ public class SwerveDrivetrain extends SubsystemBase {
 		Ports.CAN.REAR_RIGHT_DRIVING,
 		Ports.CAN.REAR_RIGHT_TURNING,
 		Ports.Analog.REAR_RIGHT_TURNING_ABSOLUTE_ENCODER,
-		false);
+		true);
 
 	// The gyro sensor
 //	private final AHRS m_gyro = new AHRS(); // usign SPI by default, which is what we want.
@@ -113,21 +112,18 @@ public class SwerveDrivetrain extends SubsystemBase {
 
 	/** Creates a new Drivetrain. */
 	public SwerveDrivetrain(Pigeon2 gyro) {
-		this.gyro=gyro;
-
-
 		m_frontLeft.calibrateVirtualPosition(FRONT_LEFT_VIRTUAL_OFFSET_RADIANS); // set virtual position for absolute encoder
 		m_frontRight.calibrateVirtualPosition(FRONT_RIGHT_VIRTUAL_OFFSET_RADIANS);
 		m_rearLeft.calibrateVirtualPosition(REAR_LEFT_VIRTUAL_OFFSET_RADIANS);
 		m_rearRight.calibrateVirtualPosition(REAR_RIGHT_VIRTUAL_OFFSET_RADIANS);
-		Timer.delay(0.50);
+
 		m_frontLeft.resetEncoders(); // resets relative encoders
 		m_frontRight.resetEncoders();
 		m_rearLeft.resetEncoders();
 		m_rearRight.resetEncoders();
-
+		this.gyro=gyro;
 		zeroHeading(); // resets gyro
-		
+
 		m_odometry = new SwerveDriveOdometry(
 		DrivetrainConstants.DRIVE_KINEMATICS,
 		Rotation2d.fromDegrees(GYRO_ORIENTATION * gyro.getAngle()),
---
+++ src/main/java/frc/robot/subsystems/apriltagvision/Vision.java
@@ -0,0 +1,85 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.apriltagvision;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Vision extends SubsystemBase {
+  /** Creates a new Vision. */
+
+  NetworkTable table;
+
+  double tv;
+  double tx;
+  double ty;
+  double ta;
+  double ts;
+  double tl;
+  double cl;
+  double getpipe;
+
+  double[] botpose;
+  double[] botpose_wpiblue;
+  double[] botpose_wpired;
+  double[] camerapose_targetspace;
+  double[] targetpose_cameraspace;
+  double[] targetpose_robotspace;
+  double[] botpose_targetspace;
+  double tid;
+
+  public Vision() {
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+
+    updateTables();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    updateTables();
+  }
+
+  public void updateTables(){
+    double def = 0.0;
+    	// Get data from NetworkTable
+			// tshort	Sidelength of shortest side of the fitted bounding box (pixels)
+			// tlong	Sidelength of longest side of the fitted bounding box (pixels)
+			// thor		Horizontal sidelength of the rough bounding box (0 - 320 pixels)
+			// tvert	Vertical sidelength of the rough bounding box (0 - 320 pixels)
+			// camtran	Results of a 3D position solution, NumberArray: Translation (x,y,z) Rotation(pitch,yaw,roll) 
+			//camerapose_robotspace	  3D transform of the camera in the coordinate system of the robot (array (6))          
+      tv = table.getEntry("tv").getDouble(def); //Whether the limelight has any valid targets (0 or 1)
+      if (tv != 0.0){
+        tx = table.getEntry("tx").getDouble(def); //Horizontal Offset From Crosshair To Target (LL1: -27 degrees to 27 degrees | LL2: -29.8 to 29.8 degrees)
+        ty = table.getEntry("ty").getDouble(def); //Vertical Offset From Crosshair To Target (LL1: -20.5 degrees to 20.5 degrees | LL2: -24.85 to 24.85 degrees)
+        ta = table.getEntry("ta").getDouble(def); //Target Area (0% of image to 100% of image)
+        ts = table.getEntry("ts").getDouble(def); //Skew or rotation (-90 degrees to 0 degrees)
+        tl = table.getEntry("tl").getDouble(def); //The pipeline’s latency contribution (ms) Add at least 11ms for image capture latency.
+        cl = table.getEntry("cl").getDouble(def);
+        getpipe = table.getEntry("getpipe").getDouble(def); //True active pipeline index of the camera (0 .. 9)
+
+        botpose = table.getEntry("botpose").getDoubleArray(new double[6]); //Robot transform in field-space. Translation (X,Y,Z) Rotation(Roll,Pitch,Yaw), total latency (cl+tl)
+        botpose_wpiblue = table.getEntry("botpose_wpiblue").getDoubleArray(new double[6]); //Robot transform in field-space (blue driverstation WPILIB origin). Translation (X,Y,Z) Rotation(Roll,Pitch,Yaw), total latency (cl+tl)
+        botpose_wpired = table.getEntry("botpose_wpired").getDoubleArray(new double[6]); //Robot transform in field-space (red driverstation WPILIB origin). Translation (X,Y,Z) Rotation(Roll,Pitch,Yaw), total latency (cl+tl)
+        camerapose_targetspace = table.getEntry("camerapose_targetspace").getDoubleArray(new double[6]); //3D transform of the camera in the coordinate system of the primary in-view AprilTag (array (6))
+        targetpose_cameraspace = table.getEntry("targetpose_cameraspace").getDoubleArray(new double[6]); //3D transform of the primary in-view AprilTag in the coordinate system of the Camera (array (6))
+        targetpose_robotspace = table.getEntry("targetpose_robotspace").getDoubleArray(new double[6]); //3D transform of the primary in-view AprilTag in the coordinate system of the Robot (array (6))
+        botpose_targetspace = table.getEntry("botpose_targetspace").getDoubleArray(new double[6]); //3D transform of the robot in the coordinate system of the primary in-view AprilTag (array (6))
+        tid = table.getEntry("tid").getDouble(def); //ID of the primary in-view AprilTag
+      }
+    
+    
+  }
+
+  public boolean hasValidTarget(){
+    boolean result = (tv != 0);
+    return result;
+  }
+
+
+}
