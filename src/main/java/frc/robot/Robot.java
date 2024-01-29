cf9036f6cb95a80819d166d729e5577894d9a632
SHAM-ROCK-BOTICS <shamrockbotics@gmail.com>
2024-01-29 18:38:34 -0500

    removed choosers from smartdashboaroooood

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -146,13 +146,6 @@ public class Robot extends TimedRobot {
 		SmartDashboard.putNumber(   "Heading",             m_robotContainer.getDrivetrain().getHeading());
 
 		SmartDashboard.putString("Auton selected", m_robotContainer.getAutonChooser().getSelected());	
-		SmartDashboard.putString("Game piece", m_robotContainer.getGamePieceChooser().getSelected());
-		SmartDashboard.putString("Start position", m_robotContainer.getStartPositionChooser().getSelected());
-		SmartDashboard.putString("Main target", m_robotContainer.getMainTargetChooser().getSelected());
-		SmartDashboard.putString("Camera option", m_robotContainer.getCameraOptionChooser().getSelected());
-		SmartDashboard.putString("Sonar option", m_robotContainer.getSonarOptionChooser().getSelected());
-		SmartDashboard.putString("Release chosen", m_robotContainer.getReleaseChooser().getSelected());
-		SmartDashboard.putString("Auton option", m_robotContainer.getAutonOptionChooser().getSelected());
 
 		SmartDashboard.putNumber("Current April Tag ID", m_robotContainer.getVision().getTagID());
 	}
