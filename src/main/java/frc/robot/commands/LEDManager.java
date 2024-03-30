a6c2d4142b43e108a3c1be505dd51ade78a5b89f
SHAM-ROCK-BOTICS <shamrockbotics@gmail.com>
2024-03-30 15:55:25 -0400

    Automatic Commit: March 30, 2024, 3:55:25 PM

--- src/main/java/frc/robot/commands/LEDManager.java
+++ src/main/java/frc/robot/commands/LEDManager.java
@@ -6,7 +6,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.LEDs.LEDConstants;
 import frc.robot.subsystems.LEDs.LEDs;
 
 public class LEDManager extends Command {
@@ -28,9 +27,11 @@ public class LEDManager extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    leds.setAllColor(LEDConstants.green);
+    // leds.setAllColor(LEDConstants.green);
+    count++;
     if (count > 30) {
       leds.solidDimRainbow();
+      count = 0;
     }
   }
 
